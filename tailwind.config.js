0bd1126a3cae1be5a5a4dcffb54f9cd558675fda
err09r <roma123makar@mail.ru>
2022-05-25 01:30:24 +0200

    Add Tailwind CSS support

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./assets/**/*.{vue,js,ts,jsx,tsx}",
+    "./templates/**/*.{html,twig}"
+
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+      }
+    },
+  },
+  plugins: [],
+}
